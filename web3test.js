2e8137050ea1cbd4054061726161efee5f675abb
tothemoon999 <tothemoon999@yandex.com>
2024-04-12 14:08:41 +0900

    aaaaaaaaaaaa

--- web3test.js
+++ web3test.js
@@ -20,28 +20,28 @@ var web3 = new Web3(new Web3.providers.WebsocketProvider(url, options));
 
 var init = async () => {
 
-    let subscription = await web3.eth.subscribe("logs", {
-        fromBlock: "newPendingTransactions",
-        address: ["0x4200000000000000000000000000000000000006"],
-        topics: [],
-    });
-
-    subscription.on("data", async (event) => {
-        const tx = await web3.eth.getTransaction(event.transactionHash);
-        console.log("tx", tx); //this is confirmed transaction because it has block number in it
-    });
-
-    // const subscription = await web3.eth.subscribe("pendingTransactions");
-    // subscription.on("data", (txHash) => {
-    //     setTimeout(async () => {
-    //         try {
-    //             let tx = await web3.eth.getTransaction(txHash);
-    //             console.log(tx)
-    //         } catch (err) {
-    //             console.error(err);
-    //         }
-    //     });
+    // let subscription = await web3.eth.subscribe("logs", {
+    //     fromBlock: "newPendingTransactions",
+    //     address: ["0x4200000000000000000000000000000000000006"],
+    //     topics: [],
     // });
+
+    // subscription.on("data", async (event) => {
+    //     const tx = await web3.eth.getTransaction(event.transactionHash);
+    //     console.log("tx", tx); //this is confirmed transaction because it has block number in it
+    // });
+
+    const subscription = await web3.eth.subscribe("newPendingTransactions");
+    subscription.on("data", (txHash) => {
+        setTimeout(async () => {
+            try {
+                let tx = await web3.eth.getTransaction(txHash);
+                console.log(tx)
+            } catch (err) {
+                console.error(err);
+            }
+        });
+    });
 };
 
 init();
