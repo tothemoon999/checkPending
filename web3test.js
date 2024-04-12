0cfb3f5fc2f38bd60f7189f15805f28ac1daa126
tothemoon999 <tothemoon999@yandex.com>
2024-04-12 14:00:00 +0900

    aaaaaaaaaaaaaaa

--- web3test.js
+++ web3test.js
@@ -20,17 +20,28 @@ var web3 = new Web3(new Web3.providers.WebsocketProvider(url, options));
 
 var init = async () => {
 
-    const subscription = await web3.eth.subscribe("pendingTransactions");
-    subscription.on("data", (txHash) => {
-        setTimeout(async () => {
-            try {
-                let tx = await web3.eth.getTransaction(txHash);
-                console.log(tx)
-            } catch (err) {
-                console.error(err);
-            }
-        });
+    let subscription = web3.eth.subscribe("logs", {
+        fromBlock: "pending",
+        address: ["0x0eD7e52944161450477ee417DE9Cd3a859b14fD0"],
+        topics: [],
     });
+
+    subscription.on("data", async (event) => {
+        const tx = await web3.eth.getTransaction(event.transactionHash);
+        console.log("tx", tx); //this is confirmed transaction because it has block number in it
+    });
+
+    // const subscription = await web3.eth.subscribe("pendingTransactions");
+    // subscription.on("data", (txHash) => {
+    //     setTimeout(async () => {
+    //         try {
+    //             let tx = await web3.eth.getTransaction(txHash);
+    //             console.log(tx)
+    //         } catch (err) {
+    //             console.error(err);
+    //         }
+    //     });
+    // });
 };
 
 init();
